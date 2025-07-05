18e54e31970ae450303e4e9839869d0d98b144b9
hatake <hermit183@outlook.com>
2025-07-05 23:37:02 +0900

    ファイル新規作成処理を追加
    その時のtokenの値を設定（ファイル存在時のtokenの値は未設定）
    free_token関数：tokenとそれに関する動的配列を開放する

--- kaname.c
+++ kaname.c
@@ -1,13 +1,30 @@
+#define TOKEN_BUFFER 1024
+#define TOKEN_STR_BUFFER 256
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+struct {
+	char *str;
+	size_t size_of;
+	enum {
+		TT_WORD,
+		TT_BLANK,
+	} type;
+} token_s;
+
+void free_token(void);
 int readkey(void); /* 環境依存 */
 
 char path[256];
+struct token_s *token;
+int size_of_token;
 
 int main(int argc, char **argv) {
 	int key;
 
+//	ファイルの内容を取得
 	if(argc < 2) {
 		FILE *file;
 		for(int i = 1; 1; i ++) {
@@ -18,7 +35,59 @@ int main(int argc, char **argv) {
 			}
 			fclose(file);
 		}
+		size_of_token = TOKEN_BUFFER;
+		token = malloc(sizeof(struct token) * size_of_token);
+		if(!token) {
+			free_token();
+			return -1;
+		}
+		token[0].size_of = TOKEN_STR_BUFFER;
+		token[0].str = malloc(sizeof(char) * token[0].size_of);
+		strcpy(token[0].str, "\n");
+		if(size_of_token < 2) {
+			void *p;
+			size_of_token += TOKEN_BUFFER;
+			p = realloc(token, size_of_token);
+			if(!p) {
+				free_token();
+				return -1;
+			}
+			for(int i = size_of_token - TOKEN_BUFFER; i < size_of_token; i ++) {
+				token[i].size_of = TOKEN_STR_BUFFER;
+				token[i].str = malloc(sizeof(char) * token[i].size_of);
+			}
+		}
+		token[1].str = NULL;
 	} else if(argc == 2) {
+		FILE *file;
+		file = fopen(path, "r");
+		if(file) {
+		//	ファイルの内容をトークン化する
+		} else {
+			size_of_token = TOKEN_BUFFER;
+			token = malloc(sizeof(struct token) * size_of_token);
+			if(!token) {
+				free_token();
+				return -1;
+			}
+			token[0].size_of = TOKEN_STR_BUFFER;
+			token[0].str = malloc(sizeof(char) * token[0].size_of);
+			strcpy(token[0].str, "\n");
+			if(size_of_token < 2) {
+				void *p;
+				size_of_token += TOKEN_BUFFER;
+				p = realloc(token, size_of_token);
+				if(!p) {
+					free_token();
+					return -1;
+				}
+				for(int i = size_of_token - TOKEN_BUFFER; i < size_of_token; i ++) {
+					token[i].size_of = TOKEN_STR_BUFFER;
+					token[i].str = malloc(sizeof(char) * token[i].size_of);
+				}
+			}
+			token[1].str = NULL;
+		}
 		strcpy(path, argv[1]);
 	} else {
 		fputs("コマンドライン引数が多すぎます", stderr);
@@ -44,7 +113,14 @@ EXIT_SUCCESS:
 	return 0;
 }
 
+void free_token(void) {
+	for(int i = 0; i < size_of_token; i ++) {
+		free(token[i].str);
+	}
+	free(token);
+}
 
+/* 以降には環境依存（linuxとWindowsに対応している）コードを記述しています */
 
 #if defined(__linux__)
 #	include <termios.h>
@@ -52,8 +128,6 @@ EXIT_SUCCESS:
 #elif defined(_WIN32) || defined(_WIN64)
 #	include<windows.h>
 #endif
-
-/* 以降には環境依存（linuxとWindowsに対応している）コードを記述しています */
 
 int readkey(void) {
 	int key;
